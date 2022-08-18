491c4c24447ba8d78a9875d641e61713bff7ec1e
sabricakir <sabricakir86@gmail.com>
2022-08-18 12:10:53 +0300

    add validates to all models

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,22 @@
 class Model < ApplicationRecord
   has_many :product
+
+  validates :name,
+            presence:true,
+            length: {maximum: 50}
+  validates :waist,
+            presence:true,
+            numericality: {greater_than: 0}
+  validates :shoulder,
+            presence:true,
+            numericality: {greater_than: 0}
+  validates :chest,
+            presence:true,
+            numericality: {greater_than: 0}
+  validates :length,
+            presence:true,
+            numericality: {greater_than: 0}
+  validates :hip,
+            presence:true,
+            numericality: {greater_than: 0}
 end
