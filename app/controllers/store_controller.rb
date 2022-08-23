b0df091719cea51c22d5cd6116bb060d15e86b50
sabricakir <sabricakir86@gmail.com>
2022-08-23 11:24:59 +0300

    add filter and search feature

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,11 +1,9 @@
 class StoreController < ApplicationController
   def index
-    if params[:gender]
-      @products = Product.where(gender: params[:gender])
-    else
-      @products = Product.all
-    end
-    @q = @products.ransack(params[:q])
-    @products = @q.result(distinct: true)
+    @products = Product.filter(filtering_params)
+  end
+
+  def filtering_params
+    params.slice(:gender, :contains, :color, :product_type, :less_than_price, :more_than_price, :price_range, :ascending_price, :descending_price)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,16 @@
 class Product < ApplicationRecord
+  include Filterable
+
+  scope :filter_by_gender, -> (gender) { where gender: gender }
+  scope :filter_by_color, -> (color) {Product.joins(:color).where(color: {name: color})}
+  scope :filter_by_product_type, -> (product_type) {Product.joins(:product_type).where(product_type: {name: product_type.gsub('-',' ')})}
+  scope :filter_by_descending_price, -> (descending_price) {order(price: :desc) if descending_price}
+  scope :filter_by_ascending_price, -> (ascending_price) {order(price: :asc) if ascending_price}
+  scope :filter_by_more_than_price, -> (price) {where("price >= ?", price)}
+  scope :filter_by_less_than_price, -> (price) {where("price <= ?", price)}
+  scope :filter_by_price_range, -> (price_range) {where("price >= ? AND price <= ?", price_range.first, price_range.last)}
+  scope :filter_by_contains, -> (name) { where("lower(name) like ?", "%#{name.downcase}%")}
+
   before_save :set_default_attributes
 
   enum gender: [:women, :men]
